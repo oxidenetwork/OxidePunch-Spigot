a5ae6d1b198ff0b16b040c01d279cd61fd6822af
deadman96385 <seanhoyt963@gmail.com>
2019-06-10 08:15:55 -0500

    Add configurable cooldown between punches and a bypass permission

--- src/main/java/com/oxide/staffpunch/RightClickListener.java
+++ src/main/java/com/oxide/staffpunch/RightClickListener.java
@@ -9,31 +9,43 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 
 public class RightClickListener implements Listener {
 
+    private ArrayList<Player> cooldown = new ArrayList<>();
     private ArrayList<Player> nofall = new ArrayList<>();
 
     @EventHandler
     public void RightClick(PlayerInteractEntityEvent event) {
         Player player = event.getPlayer();
-        Player click_player = ((Player) event.getRightClicked());;
+        Player click_player = ((Player) event.getRightClicked());
         if (!player.hasPermission("oxidepunch.use") || !click_player.hasPermission("oxidepunch.use")) {
             return;
         }
+        if (cooldown.contains(player)) {
+            player.sendMessage(org.bukkit.ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("cooldown-message")));
+            return;
+        }
         if (event.getRightClicked() instanceof Player) {
+            int cooldowntime = Main.plugin.getConfig().getInt("cooldowntime");
             if (event.getHand() == EquipmentSlot.HAND) {
                 Bukkit.getServer().broadcastMessage(ChatColor.translateAlternateColorCodes('&', Main.plugin.getConfig().getString("message").replaceAll("%player%", player.getDisplayName()).replaceAll("%click_player%", click_player.getDisplayName())));
                 click_player.setVelocity(new Vector(0, Main.plugin.getConfig().getInt("launch-power"), 0));
+                cooldown.add(player);
+                if (player.hasPermission("oxidepunch.bypasscooldown")) {
+                    cooldown.remove(player);
+                }
                 if (!nofall.contains(click_player)) {
                     nofall.add(click_player);
                 }
-                fireworkgen(click_player, click_player.getLocation(), Main.plugin.getConfig().getInt("amount-of-fireworks"), Main.plugin.getConfig().getInt("flight-time-of-fireworks"));
-
+                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask((Plugin) this, () -> cooldown.remove(player), cooldowntime * 20);
             }
+            fireworkgen(click_player, click_player.getLocation(), Main.plugin.getConfig().getInt("amount-of-fireworks"), Main.plugin.getConfig().getInt("flight-time-of-fireworks"));
+
         }
     }
 
